2e3b3ba9baea9bb5dfea64ba76b1be6d724f670b
Darshan R.M <90130734+darshancpu@users.noreply.github.com>
2022-09-19 00:50:15 +0530

    some changes in the code base

--- 1.C_Program/000_Practise.c
+++ 1.C_Program/000_Practise.c
@@ -1,32 +1,21 @@
-// to print the candidates who won the election
-#include<stdio.h>
-struct election
-{
-    char name[20];
-    int votes;
-};
-int main()
-{
-    int n ;
-    printf("Enter number of candidaties in election : ");
-    scanf("%d",&n);
-    ele e[n];
-    for(int i=0;i<n;i++)
+#include <stdio.h> // header file for Standard Input Output
+#include <stdlib.h> // header file for Standard Library
+
+int main() {
+
+    int G , S , A ,B ;
+    scanf("%d %d %d %d",&G,&S,&A,&B);
+    int gold_cost = G*A ;
+    int silver_cost= S*B;
+    if (gold_cost>=silver_cost)
     {
-        printf("Enter the name of candidate %d : ",i+1);
-        scanf("%s",&e[i].name);
-        printf("Enter the number of votes of the candidate %d : ",i+1);
-        scanf("%d",&e[i].votes);
+        printf("Gold");
     }
-    int max_Votes = e[0].votes, index_Count = 0;
-    for(int i =0;i<n;i++)
+    else
     {
-        if(max_Votes<=e[i].votes)
-        {
-            index_Count = i ;
-        }
+        printf("Silver");
     }
-    printf("The winner of the election is %s and number of votes are %d : ",e[index_Count].name,e[index_Count].votes);
-    
-    return 0 ;
+
+    //Your code here
+    return 0;
 }
--- 4.Phyton_Program/000_Practise.py
+++ 4.Phyton_Program/000_Practise.py
@@ -1,15 +1,38 @@
-from lib2to3.pytree import type_repr
+import math
+import os
+import random
+import re
+import sys
 
 
-a=[1,2,3,'4']
-def sum(n):
-    t=0
-    try:
-        for i in n:
-            t+=i
-        print(t)
-    except:
-        print("sone")
-    except TypeError:
-        print("w")
-sum(a)
+class Car:
+    max_speed = 0 
+    speed_unit = ""
+    def Car (max_speed,speed_unit):
+        if speed_unit == "km/h":
+            print(f"Car with the maximum speed of {max_speed} km/h") 
+        else :
+            print(f"Car with the maximum speed of {max_speed} mph")
+
+class Boat:
+    max_speed = 0 
+    def Boat(max_speed):
+        print(f"Boat with the maximum speed of {max_speed} knots")
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input())
+    queries = []
+    for _ in range(q):
+        args = input().split()
+        vehicle_type, params = args[0], args[1:]
+        if vehicle_type == "car":
+            max_speed, speed_unit = int(params[0]), params[1]
+            vehicle = Car(max_speed, speed_unit)
+        elif vehicle_type == "boat":
+            max_speed = int(params[0])
+            vehicle = Boat(max_speed)
+        else:
+            raise ValueError("invalid vehicle type")
+        fptr.write("%s\n" % vehicle)
+    fptr.close()
